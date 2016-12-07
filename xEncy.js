3f2c7a1c02096486076b8c9dd2b8d42def970c43
flipog <flipog@hotmail.com>
2016-12-07 19:55:12 -0200

    0005
    a . Minor changes that seem big (mostly at Index and Style)
    b . Removed useless call at vFunc.js
    c . ‘aToZ’ is a simple visual alphabet
    d . ‘info.gps’ at xEncy.js holds the coordinates to the arena cells
    e . Athletes now have their position and behavior nested in ‘change’ at
    yChan.js

--- xEncy.js
+++ xEncy.js
@@ -3,7 +3,7 @@
 
 //////////////////////////////////////////////////////////////////////////////// GLOBALS
 //
-// DISPLAY LISTEN PATH TEXT PAGE WORLD TEXT EVENT CHANGE STATE ENGINE
+// GAME LISTEN TEXT PAGE ATOZ INFO TEXT EVENT CHANGE STATE ENGINE
 
 //////////////////////////////////////////////////////////////////////////////// PAGE
 //
@@ -17,32 +17,92 @@ const page =
   //////////////////////////////////////////////////////////////////////////////
   // Get HTML elements
   //
-  player:
+  athlete:
   [
-    document.getElementById( 'P01' ),
-    document.getElementById( 'P02' ),
-    document.getElementById( 'P03' ),
-    document.getElementById( 'P04' ),
-    document.getElementById( 'P05' ),
-    document.getElementById( 'P06' ),
-    document.getElementById( 'P07' ),
-    document.getElementById( 'P08' ),
-    document.getElementById( 'P09' ),
-    document.getElementById( 'P10' ),
-    document.getElementById( 'P11' ),
-    document.getElementById( 'P12' ),
-    document.getElementById( 'P13' ),
-    document.getElementById( 'P14' ),
-    document.getElementById( 'P15' ),
-    document.getElementById( 'P16' ),
-    document.getElementById( 'P17' ),
-    document.getElementById( 'P18' ),
-    document.getElementById( 'P19' ),
-    document.getElementById( 'P20' ),
+    document.getElementById( 'T00' ),
+    document.getElementById( 'T01' ),
+    document.getElementById( 'T02' ),
+    document.getElementById( 'T03' ),
+    document.getElementById( 'T04' ),
+    document.getElementById( 'T05' ),
+    document.getElementById( 'T06' ),
+    document.getElementById( 'T07' ),
+    document.getElementById( 'T08' ),
+    document.getElementById( 'T09' ),
+    document.getElementById( 'T10' ),
+    document.getElementById( 'T11' ),
+    document.getElementById( 'T12' ),
+    document.getElementById( 'T13' ),
+    document.getElementById( 'T14' ),
+    document.getElementById( 'T15' ),
+    document.getElementById( 'T16' ),
+    document.getElementById( 'T17' ),
+    document.getElementById( 'T18' ),
+    document.getElementById( 'T19' ),
   ]
 }
 
-//////////////////////////////////////////////////////////////////////////////// WORLD
+//////////////////////////////////////////////////////////////////////////////// ATOZ
+// ez2uz alphabet
 //
-const world =
-{}
+const aToZ =
+[
+  'A','B','C','D',
+  'E','F','G','H',
+  'I','J','K','L',
+  'M','N','O','P',
+  'Q','R','S','T',
+  'U','V','W','X',
+  'Y','Z',
+]
+
+//////////////////////////////////////////////////////////////////////////////// INFO
+//
+const info =
+{
+  //////////////////////////////////////////////////////////////////////////////
+  //
+  family:
+  {},
+
+  //////////////////////////////////////////////////////////////////////////////
+  // GPS is an array containing 13 arrays (arena rows)
+  // each of these arrays cointain 20 arrays (row cells coordinates)
+  // each of these arrays cointain 1 string and 2 numbers (x, y, name)
+  //
+  gps:
+  (
+    function()
+    {
+      let array1 = []
+      let array2 = []
+
+      for( let $1 = 0; $1 < 13; $1 ++ ) // insert 13 rows into array1
+      {
+        for( let $2 = 0; $2 < 20; $2 ++ ) // insert 20 cells into array2
+        {
+          let x = $2 * 48
+          let y = $1 * 48
+
+          //--------------------------------------------------------------------
+          // Corrects number appearance if necessary then writes name
+          //
+          let name = aToZ[ $1 ]
+          if( $2 < 10 ) name += '0'
+          name += $2
+
+          //--------------------------------------------------------------------
+          // Corrects vertical coordinate if necessary
+          //
+          if( $1 === 12 ) y += 7
+
+          array2.push( [ x, y, name ] )
+        }
+
+        array1.push( array2 )
+      }
+
+      return array1
+    }()
+  ),
+}
--- yChan.js
+++ yChan.js
@@ -3,9 +3,29 @@
 
 //////////////////////////////////////////////////////////////////////////////// GLOBALS
 //
-// DISPLAY LISTEN PATH TEXT PAGE WORLD TEXT EVENT CHANGE STATE ENGINE
+// GAME LISTEN TEXT PAGE ATOZ INFO TEXT EVENT CHANGE STATE ENGINE
 
 //////////////////////////////////////////////////////////////////////////////// CHANGE
 //
 let change =
-{}
+{
+  //============================================================================
+  //
+  turn: 0,
+
+  //============================================================================
+  // ATHLETE is an array containing 20 arrays
+  // each of these arrays contain 3 numbers (x, y, color)
+  //
+  athlete:
+  (
+    function()
+    {
+      let array = []
+
+      for( let $ = 0; $ < 20; $ ++ ) array.push( [ 0, 0, 0 ] )
+
+      return array
+    }()
+  ),
+}

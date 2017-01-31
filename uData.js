63614e484211d3d5d6c90b21d45f7e7c429612d8
flipog <flipog@hotmail.com>
2017-01-31 16:18:05 -0200

    99% file name changes
    a . yLstn.js is now 2 separate files, “wClic.js” and “xHove.js“
    b . There are now important “setTimeouts” used to prevent exploitation
    of fast clicking under wClic.js

---
+++ uData.js
@@ -0,0 +1,273 @@
+
+'use strict'
+
+////////////////////////////////////////////////////////////////////////////////
+// Every mutable data
+//
+Ω.state =
+{
+  //============================================================================
+  // Distance between the edge of the screen and the arena
+  //
+  screen: { x: 0, y: 0 },
+
+  //============================================================================
+  // Prevents fast click exploit
+  //
+  lock: false,
+
+  //............................................................................
+  // Shows how many plays have happened
+  //
+  turn: 0,
+
+  //============================================================================
+  // Shows which players are in which team
+  //
+  team: { green: [], blue: [], playing: [] },
+
+  //............................................................................
+  // Which, if any, is the current goalkeeper of each team
+  //
+  keeper: { green: 'none', blue: 'none' },
+
+  //============================================================================
+  // How many replacements each player still has
+  //
+  reps: { green: 2, blue: 2 },
+
+  //............................................................................
+  // Shows which athletes have been replaced
+  //
+  outed: [],
+
+  //============================================================================
+  // Shows which athletes are in the roundabout and which is currently moving
+  //
+  rounded: [],
+  rounding: [ 'none', 0 ],
+
+  //============================================================================
+  // Which player played first and which is to play now
+  //
+  firstPlayer: '', // 'gre' or 'blu'
+  currentPlayer: '', // 'gre' or 'blu'
+
+  //============================================================================
+  // Shows which athlete is being displayed
+  //
+  selected: 'none', // 'none', 'ball' or 0 to 19
+  hovered: 'none', // 'none', 'ball' or 0 to 19
+
+  displayed: 'none', // 'none', 'ball' or 0 to 19
+
+  //============================================================================
+  // Shows which athlete is being pushed
+  //
+  pushed: 'none', // 'none' or 0 to 19
+
+  //============================================================================
+  // Shows which athlete is holding the ball, and if there is someone taking it
+  //
+  holder: 'none', // 'none' or 0 to 19
+  newHolder: 'none', // 'none' or 0 to 19
+
+  //============================================================================
+  // Shows which zones are not pushable
+  //
+  blocked: [],
+
+  //============================================================================
+  // Shows which athletes may not be selected
+  //
+  marked: [],
+
+  //============================================================================
+  // Defines which athletes (and/or the ball), if any, is under a zone
+  //
+  target: { zone: [], aimed: [] },
+
+  //============================================================================
+  // Ball's current position
+  //
+  ball: { x: 0, y: 0 },
+
+  //============================================================================
+  // Athletes' positions and colors
+  //
+  athlete:
+  (
+    function()
+    {
+      let array = []
+
+      for( let $ = 0; $ < 20; $ ++ ) array.push( { x: 0, y: 0, color: 'none' } )
+
+      return array
+    }()
+  ),
+
+  //============================================================================
+  // Zones' positions
+  //
+  zone:
+  (
+    function()
+    {
+      let array = []
+
+      for( let $ = 0; $ < 16; $ ++ ) array.push( { x: 'none', y: 'none' } )
+
+      return array
+    }()
+  ),
+
+  //============================================================================
+  // Spawn points of each team
+  //
+  spawn:
+  {
+    green: [ 'C02', 'D03', 'I03', 'J02' ],
+    blue: [ 'J17', 'I16', 'D16', 'C17' ],
+  },
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Every immutable data
+//
+Ω.info =
+{
+  //============================================================================
+  // Matrix of how each athlete moves
+  //
+  // 00 01 02 03
+  //    04 05 06
+  //       07 08
+  //          09
+  //
+  // 00 is how many zones the athlete needs
+  // It also cosmetically represents the athlete's position in the matrix
+  //
+  move:
+  [
+    [ 16, 1,0,0, 1,1,0, 0,0, 0 ], // T00
+
+    [ 12, 0,1,0, 0,1,0, 0,0, 0 ], // T01
+    [ 12, 0,1,0, 0,1,0, 0,0, 0 ], // T02
+
+    [ 12, 0,0,0, 0,1,0, 1,0, 0 ], // T03
+
+    [ 12, 1,1,0, 1,0,0, 0,0, 0 ], // T04
+
+    [ 8, 0,0,0, 0,1,0, 0,0, 0 ], // T05
+    [ 8, 0,0,0, 0,1,0, 0,0, 0 ], // T06
+
+    [ 8, 0,0,0, 0,0,1, 0,0, 0 ], // T07
+    [ 8, 0,0,0, 0,0,1, 0,0, 0 ], // T08
+    [ 8, 0,0,0, 0,0,1, 0,0, 0 ], // T09
+    [ 8, 0,0,0, 0,0,1, 0,0, 0 ], // T10
+
+    [ 8, 0,0,0, 0,0,0, 0,1, 0 ], // T11
+    [ 8, 0,0,0, 0,0,0, 0,1, 0 ], // T12
+
+    [ 4, 0,0,0, 0,0,0, 0,0, 1 ], // T13
+
+    [ 4,0,0,1, 0,0,0, 0,0, 0 ], // T14
+    [ 4,0,0,1, 0,0,0, 0,0, 0 ], // T15
+    [ 4,0,0,1, 0,0,0, 0,0, 0 ], // T16
+
+    [ 4,0,0,0, 0,0,0, 1,0, 0 ], // T17
+
+    [ 8,1,0,0, 1,0,0, 0,0, 0 ], // T18
+
+    [ 4,0,0,0, 1,0,0, 0,0, 0 ], // T19
+  ],
+
+  //============================================================================
+  //
+  aToL: [ 'A','B','C','D','E','F','G','H','I','J','K','L' ],
+
+  //============================================================================
+  // The 4 cells that make up the arena's center
+  //
+  arenaCenter: [ 'F09', 'F10', 'G09', 'G10' ],
+
+  //============================================================================
+  // Each is an array of cell names relative to the area
+  //
+  area:
+  {
+    //..........................................................................
+    //
+    green:
+    (
+      function()
+      {
+        let array = []
+
+        for( let $ = 0; $ < Ω.page.areaGreen.length; $ ++ )
+        {
+          array.push( Ω.page.areaGreen[ $ ].id )
+        }
+
+        return array
+      }()
+    ),
+
+    //..........................................................................
+    //
+    blue:
+    (
+      function()
+      {
+        let array = []
+
+        for( let $ = 0; $ < Ω.page.areaBlue.length; $ ++ )
+        {
+          array.push( Ω.page.areaBlue[ $ ].id )
+        }
+
+        return array
+      }()
+    ),
+  },
+
+  //============================================================================
+  // 'info.cell' is an array containing 13 arrays (arena rows)
+  // Each of these arrays contain 20 arrays (row's cells coordinates)
+  //
+  cell:
+  (
+    function()
+    {
+      //========================================================================
+      // Insert 13 rows into arrayRows
+      //
+      let arrayRows = []
+
+      for( let $1 = 0; $1 < 13; $1 ++ )
+      {
+        //......................................................................
+        // Insert 20 cells into arrayCells
+        //
+        let arrayCells = []
+
+        for( let $2 = 0; $2 < 20; $2 ++ )
+        {
+          let x = $2 * 48
+          let y = $1 * 48
+
+          if( $1 === 12 ) y += 7 // correction for the bench
+
+          arrayCells.push( { x: x, y: y } )
+        }
+
+        arrayRows.push( arrayCells )
+      }
+
+      //========================================================================
+      //
+      return arrayRows
+    }()
+  ),
+}
---
+++ xHove.js
@@ -0,0 +1,321 @@
+
+'use strict'
+
+////////////////////////////////////////////////////////////////////////////////
+// Tracks hover actions in the game
+//
+Ω.hoverer = addEventListener( 'mouseover', function( $ )
+{
+  //////////////////////////////////////////////////////////////////////////////
+  // Refresh hovered
+  //
+  Ω.state.hovered = 'none'
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Hover color . Part 1 . Refresh everything
+  //
+  Ω.page.ball.style.backgroundColor = 'rgb(111,79,47)'
+
+  for( let $1 = 0; $1 < 20; $1 ++ )
+  {
+    let athleteColor = Ω.state.athlete[ $1 ].color
+    let darkerColor
+
+    //==========================================================================
+    // Teamless athlete
+    //
+    if( athleteColor === 'none' ) darkerColor = 'rgb(143,143,143)'
+
+    //..........................................................................
+    // Athlete is playing or was benched this match
+    //
+    else if( athleteColor === 'gre' || athleteColor === 'greBlk' )
+    {
+      darkerColor = 'rgb(127,175,47)'
+    }
+
+    else if( athleteColor === 'blu' || athleteColor === 'bluBlk' )
+    {
+      darkerColor = 'rgb(95,63,191)'
+    }
+
+    //==========================================================================
+    // Set the athlete's color
+    //
+    Ω.page.athlete[ $1 ].style.backgroundColor = darkerColor
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // 00 . Hover the ball (if unmarked)
+  //
+  if( $.target.id === 'ball'
+  && Ω.state.marked.indexOf( 'ball' ) === -1 )
+  {
+    Ω.state.hovered = 'ball'
+    Ω.state.displayed = Ω.state.hovered
+
+    //==========================================================================
+    // Hover color . Part 2. Change the hovered ball's color
+    //
+    Ω.page.ball.style.backgroundColor = 'rgb(143,111,79)'
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // 01 . Hover some athlete
+  //
+  else if( $.target.id.substring( 0, 3 ) === 'min' )
+  {
+    let athlete = Number( $.target.id.substring( 4, 6 ) )
+    let athleteColor = Ω.state.athlete[ athlete ].color
+    let lighterColor
+
+    //==========================================================================
+    // (if unmarked)
+    //
+    if( Ω.state.marked.indexOf( athlete ) === -1 )
+    {
+      Ω.state.hovered = athlete
+      Ω.state.displayed = Ω.state.hovered
+    }
+
+    //==========================================================================
+    // Hover color effects . Part 3 . Change the hovered athlete's color
+    //==========================================================================
+    // Teamless athlete
+    //
+    if( athleteColor === 'none' ) lighterColor = 'rgb(191,191,191)'
+
+    //..........................................................................
+    // Athlete is playing or was benched this match
+    //
+    else if( athleteColor === 'gre' || athleteColor === 'greBlk' )
+    {
+      lighterColor = 'rgb(143,191,63)'
+    }
+
+    else if( athleteColor === 'blu' || athleteColor === 'bluBlk' )
+    {
+      lighterColor = 'rgb(111,79,207)'
+    }
+
+    //==========================================================================
+    // Set the athlete's color
+    //
+    Ω.page.athlete[ athlete ].style.backgroundColor = lighterColor
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // 02 . Hover the selection zone
+  //
+  else if( $.target.id === 'selection' )
+  {
+    //==========================================================================
+    // Hover color . Part 4 . Hover whatever is below the selection zone
+    //==========================================================================
+    // If the ball is displayed
+    //
+    if( Ω.state.displayed === 'ball' )
+    {
+      Ω.page.ball.style.backgroundColor = 'rgb(143,111,79)'
+    }
+
+    //==========================================================================
+    // If an athlete is displayed
+    //
+    else if( Ω.state.displayed !== 'none' )
+    {
+      let athlete = Ω.state.displayed
+      let athleteColor = Ω.state.athlete[ athlete ].color
+      let lighterColor
+
+      //........................................................................
+      // Teamless athlete
+      //
+      if( athleteColor === 'none' ) lighterColor = 'rgb(191,191,191)'
+
+      // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+      // Athlete is playing or was benched this match
+      //
+      else if( athleteColor === 'gre' || athleteColor === 'greBlk' )
+      {
+        lighterColor = 'rgb(143,191,63)'
+      }
+
+      // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+      //
+      else if( athleteColor === 'blu' || athleteColor === 'bluBlk' )
+      {
+        lighterColor = 'rgb(111,79,207)'
+      }
+
+      //........................................................................
+      // Set the athlete's color
+      //
+      Ω.page.athlete[ athlete ].style.backgroundColor = lighterColor
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // 03 . Hover a zone
+  //
+  else if( $.target.id.substring( 0, 3 ) === 'zon' )
+  {
+    if( Ω.state.selected === 'none' ) Ω.state.displayed = 'none'
+
+    let zone = Number( $.target.id.substring( 3, 5 ) )
+    let zoneIndex = Ω.state.target.zone.indexOf( zone )
+
+    //==========================================================================
+    // HAS target
+    //
+    if( zoneIndex !== -1 ) // zone is targeting
+    {
+      let aimed = Ω.state.target.aimed[ zoneIndex ]
+
+      //........................................................................
+      // Hover color . Part 5 . Get whatever is below the zone
+      //........................................................................
+      // If the ball is being aimed
+      //
+      if( aimed === 'ball' )
+      {
+        Ω.page.ball.style.backgroundColor = 'rgb(143,111,79)'
+      }
+
+      //........................................................................
+      // If an athlete is being aimed
+      //
+      else
+      {
+        let athlete = aimed
+        let athleteColor = Ω.state.athlete[ aimed ].color
+        let lighterColor
+
+        // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        // Hover color . Part 6 . Colorize aimed
+        // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        // Teamless athlete
+        //
+        if( athleteColor === 'none' ) lighterColor = 'rgb(191,191,191)'
+
+        //  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        // Athlete is playing or was benched this match
+        //
+        else if( athleteColor === 'gre' || athleteColor === 'greBlk' )
+        {
+          lighterColor = 'rgb(143,191,63)'
+        }
+
+        else if( athleteColor === 'blu' || athleteColor === 'bluBlk' )
+        {
+          lighterColor = 'rgb(111,79,207)'
+        }
+
+        // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        // Set the athlete's color
+        //
+        Ω.page.athlete[ athlete ].style.backgroundColor = lighterColor
+      }
+    }
+
+    //==========================================================================
+    // Has NO target
+    //
+    else
+    {
+      //........................................................................
+      //
+      Ω.state.hovered = 'none'
+
+      if( Ω.state.selected !== 'none' ) Ω.state.displayed = Ω.state.selected
+      else                              Ω.state.displayed = Ω.state.hovered
+
+      //........................................................................
+      // Hover color . Part 7a . Refresh everything again
+      //
+      Ω.page.ball.style.backgroundColor = 'rgb(111,79,47)'
+
+      for( let $1 = 0; $1 < 20; $1 ++ )
+      {
+        let athlete = Ω.state.athlete[ $1 ]
+        let athleteColor = Ω.state.athlete[ $1 ].color
+        let darkerColor
+
+        // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        // Teamless athlete
+        //
+        if( athleteColor === 'none' ) darkerColor = 'rgb(143,143,143)'
+
+        // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        // Athlete is playing or was benched this match
+        //
+        else if( athleteColor === 'gre' || athleteColor === 'greBlk' )
+        {
+          darkerColor = 'rgb(127,175,47)'
+        }
+
+        else if( athleteColor === 'blu' || athleteColor === 'bluBlk' )
+        {
+          darkerColor = 'rgb(95,63,191)'
+        }
+
+        // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+        // Set the athlete's color
+        //
+        Ω.page.athlete[ $1 ].style.backgroundColor = darkerColor
+      }
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // 04 . Hover anything else
+  //
+  else
+  {
+    //==========================================================================
+    //
+    Ω.state.hovered = 'none'
+
+    if( Ω.state.selected !== 'none' ) Ω.state.displayed = Ω.state.selected
+    else                              Ω.state.displayed = Ω.state.hovered
+
+    //==========================================================================
+    // Hover color . Part 7b . Refresh everything again
+    //
+    Ω.page.ball.style.backgroundColor = 'rgb(111,79,47)'
+
+    for( let $1 = 0; $1 < 20; $1 ++ )
+    {
+      let athlete = Ω.state.athlete[ $1 ]
+      let athleteColor = Ω.state.athlete[ $1 ].color
+      let darkerColor
+
+      //........................................................................
+      // Teamless athlete
+      //
+      if( athleteColor === 'none' ) darkerColor = 'rgb(143,143,143)'
+
+      // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+      // Athlete is playing or was benched this match
+      //
+      else if( athleteColor === 'gre' || athleteColor === 'greBlk' )
+      {
+        darkerColor = 'rgb(127,175,47)'
+      }
+
+      else if( athleteColor === 'blu' || athleteColor === 'bluBlk' )
+      {
+        darkerColor = 'rgb(95,63,191)'
+      }
+
+      //........................................................................
+      // Set the athlete's color
+      //
+      Ω.page.athlete[ $1 ].style.backgroundColor = darkerColor
+    }
+  }
+
+////////////////////////////////////////////////////////////////////////////////
+// END of 'listen.hoverer'
+//
+}, false )
